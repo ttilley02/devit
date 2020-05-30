8f595528de426a537c90a7cf8c73b9c8d7b488b7
Marlo <marlovigor26@gmail.com>
2020-05-30 08:55:56 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const helmet = require('helmet')
+
+const app = express()
+
+const morganOption = (process.env.NODE_ENV === 'production')
+    ? 'tiny'
+    : 'common';
+
+app.use(morgan(morganOption))
+app.use(helmet())
+app.use(cors())
+
+app.get('/', (req, res) => {
+
+    res.send('Hello, boilerplate!')
+
+})
+
+module.exports = app
