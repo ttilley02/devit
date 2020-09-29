680314eca9086548d33eea10be965398215e47d5
Chris Mojekwu <chrismojekwu@gmail.com>
2020-09-29 18:08:52 -0500

    altered profile table added user projects table

--- migrations/002.do.create_developit_profiles.sql
+++ migrations/002.do.create_developit_profiles.sql
@@ -1,7 +1,8 @@
 CREATE TABLE developit_profiles
 (
     id SERIAL PRIMARY KEY,
-    blurb TEXT,
+    dev_blurb TEXT,
+    emp_blurb TEXT,
     projects TEXT,
     image TEXT,
     user_id INTEGER
---
+++ migrations/009.do.create_developit_user_projects.sql
@@ -0,0 +1,6 @@
+CREATE TABLE developit_user_projects (
+        id SERIAL PRIMARY KEY,
+        dev_id INTEGER
+        REFERENCES developit_users(id) ON DELETE CASCADE NOT NULL,
+        details TEXT,       
+);
