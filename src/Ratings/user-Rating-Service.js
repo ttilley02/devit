27dc78ffad5f139408701faf5e1c0d049c950123
marlo <apple@marlo-MacBook-Pro.local>
2020-10-03 11:40:29 -0400

    marlo

--- src/Ratings/user-Rating-Service.js
+++ src/Ratings/user-Rating-Service.js
@@ -2,15 +2,15 @@ const UserRatingService = {
    getRatings(knex) {
       return knex.select("*").from("developit_user_reviews");
    },
-   // insertItem(knex, newItem) {
-   //     return knex
-   //         .insert(newItem)
-   //         .into('cart')
-   //         .returning('*')
-   //         .then(rows => {
-   //             return rows[0]
-   //         })
-   // },
+   insertRating(knex, newRating) {
+       return knex
+           .insert(newRating)
+           .into('cart')
+           .returning('*')
+           .then(rows => {
+               return rows[0]
+           })
+   },
 };
 
 module.exports = UserRatingService;
